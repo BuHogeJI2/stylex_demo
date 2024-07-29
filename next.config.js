b6d7259a401f8eb14ccbf7deee76be921ef7ca4a
Dmitry Demidovich <d.demidovich@gpsolutions.com>
2024-07-29 18:47:39 +0400

    Remove mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const stylexPlugin = require("@stylexjs/nextjs-plugin");
-const withMDX = require("@next/mdx")();
 
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
@@ -9,4 +8,4 @@ const nextConfig = {
 module.exports = stylexPlugin({
   rootDir: __dirname,
   dev: process.env.NODE_ENV === 'development',
-})(withMDX(nextConfig));
+})(nextConfig);
